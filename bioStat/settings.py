214b68eda56ca67dae39b1e0a17b9cfd6941291d
Marin260 <60893098+Marin260@users.noreply.github.com>
2022-08-30 23:40:11 +0200

    change to env vars

--- bioStat/settings.py
+++ bioStat/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e%d-=+y7t3jym-^q^(kkrsbd&o0zo+)^2#o_sn^@(-lu6no-hu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_MODE')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'biostat.herokuapp.com']
 
